56695a06bb889c101c5d19b40d4918b95df4ba7d
Dbrice13 <danielle.rice@mail.mcgill.ca>
2016-05-06 13:05:22 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
-# Run_analysis.R 
+Run_analysis.R 
 
-## Preparatory work
+Preparatory work
 
 Download and unzip the dataset:
 filename <- "getdata_dataset.zip"
@@ -61,24 +61,25 @@ saveresults <- function (data,name){
 	write.csv(data,file)
 }
 
-## Required Project Steps**
 
-#### **1)Merges the training and the test sets to create one dataset.**
+**Required Project Steps**
+
+1)Merges the training and the test sets to create one dataset.
 library(plyr)
 data <- rbind(train, test)
 data <- arrange(data, id)
 
-#### **2)Extracts only the measurements on the mean and standard deviation for each measurement.** 
+2)Extracts only the measurements on the mean and standard deviation for each measurement.
 mean_and_std <- data[,c(1,2,grep("std", colnames(data)), grep("mean", colnames(data)))]
 saveresults(mean_and_std,"mean_and_std")
 
-#### **3)Uses descriptive activity names to name the activities in the dataset.**
+3)Uses descriptive activity names to name the activities in the dataset.
 activity_labels <- gettables("activity_labels.txt")
 
-#### **4)**Appropriately labels the data set with descriptive variable names. 
+4)Appropriately labels the data set with descriptive variable names. 
 data$activity <- factor(data$activity, levels=activity_labels$V1, labels=activity_labels$V2)
 
-#### **5)**Creates a second, independent tidy data set with the average of each variable for each activity and each subject.** 
+5)Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 tidy_dataset <- ddply(mean_and_std, .(id, activity), .fun=function(x){ colMeans(x[,-c(1:2)]) })
 colnames(tidy_dataset)[-c(1:2)] <- paste(colnames(tidy_dataset)[-c(1:2)], "_mean", sep="")
 saveresults(tidy_dataset,"tidy_dataset")
